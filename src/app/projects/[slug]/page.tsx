6d27eaa29af67fe058b344622c3cbebb3db632a8
asaprosa <cooldudeadi20@gmail.com>
2026-08-24 00:24:37 +0530

    added text animation and other from framer

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -40,7 +40,7 @@ export default async function ProjectPage({ params }: PageProps) {
 
   return (
     <>
-      <div>Nav test</div>
+      
       <main className="px-6 pb-28 pt-32 md:px-10">
         <div className="mx-auto max-w-content">
           <Link
